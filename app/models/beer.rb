d065781b0e54ae7051ff928c3309522bf918ac58
TuukkaP <tuukka.peuraniemi@gmail.com>
2014-01-23 01:47:25 +0200

    Week2 assignments in progress

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,15 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    #@sum = 0
+    #  ratings.each do |r|
+    #    @sum += r.score
+    #end
+    #return @sum/ratings.count
+
+
+    return ratings.average('score')
+    end
 end
